9ec736eba87cb9cc7fe83a2078fa6294194726ca
Naman Jindal <namanj@live.com>
2018-04-01 22:34:38 -0500

    Added reshapes for type flexibility

--- jacoKinematics.py
+++ jacoKinematics.py
@@ -10,12 +10,14 @@ __jaco_zero_M = getJacoZeroPose()
 
 
 def jaco_FK(thetas):
-	return evalT(__jaco_screw, thetas, __jaco_zero_M)
+	thetas = np.reshape(thetas, (6,1)) 
+    return evalT(__jaco_screw, thetas, __jaco_zero_M)
 
 def jaco_IK(endPose, theta_init = None):
-	theta, err = findIK(endPose, __jaco_screw, __jaco_zero_M, theta=theta_init, max_err = 0.01)
-	if err > 0.01:
-		# theta, err = findIK(endPose, __jaco_screw, __jaco_zero_M, theta=theta_init, max_iter=500, max_err = 0.01)
-		# if err > 0.01:
-		return None
-	return theta
+	endPose = np.reshape(endPose, (4,4)) 
+    theta, err = findIK(endPose, __jaco_screw, __jaco_zero_M, theta=theta_init, max_err = 0.01)
+    if err > 0.01:
+        theta, err = findIK(endPose, __jaco_screw, __jaco_zero_M, theta=theta_init, max_iter=5000, max_err = 0.01)
+        if err > 0.01:
+            return None
+    return theta
