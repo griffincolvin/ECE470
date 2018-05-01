19c4e63941c478eff1f7e6528b856ca27719983e
Naman Jindal <namanj@live.com>
2018-04-30 22:16:02 -0500

    added interpolation movement

--- jacoKinematics.py
+++ jacoKinematics.py
@@ -5,6 +5,7 @@ from vrepHelpers import *
 from mathHelpers import *
 from scipy.linalg import expm, sinm, cosm
 from jacoData import JacoScrewMatrix, getJacoZeroPose
+from ece470_lib import *
 import time
 
 def static_vars(**kwargs):
@@ -38,14 +39,26 @@ def jaco_move_theta(clientID, thetas, delay=0.1,p=False):
         #     print("Joint " + str(i+1) + " Moved by " + str(rad2deg(thetas[i])) + " Degrees")
         # if delay > 0:
         #     time.sleep(delay)
-    time.sleep(0.02*np.linalg.norm(thetas))
+    time.sleep(delay)
 
 
-@static_vars(__old_thetas=None)
-def jaco_move_pose(clientID, Pose, delay=0.1,p=False):
+@static_vars(__old_thetas=np.zeros(6,1))
+def jaco_move_pose(clientID, Pose, delay=0.0,p=False):
     thetas = jaco_IK(Pose, theta_init=jaco_move_pose.__old_thetas)
     if thetas is None:
         return False
+    delay2 = __old_thetas - thetas
+    delay2 = np.norm(delay) * 0.02
     jaco_move_pose.__old_thetas = thetas
-    jaco_move_theta(clientID, thetas, delay=delay, p=p)
+    jaco_move_theta(clientID, thetas, delay=delay2)
+    return True
+
+@static_vars(__old_pose = np.identity(4))
+def jaco_move_pose_interplote(clientID, new_pose, spacing=0.1, delay=0):
+    dist = np.linalg.norm(fromPose(__old_pose)[1] - fromPose(new_pose))
+    for int_pose in matrix_linspace(__old_pose, new_pose,1+int(dist/spacing) ,to_end=True):
+        if not jaco_move_pose(clientID, int_pose, delay=delay):
+            __old_pose = int_pose
+            return False
+    __old_pose = new_pose
     return True
